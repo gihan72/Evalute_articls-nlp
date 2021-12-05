bf3bfdf2e27e4493f8541a5c94bea5cc731ec031
gihan72 <gihan1972gigi@gmail.com>
2021-12-05 12:13:03 +0200

    Create testHandleSubmit.spec.js

---
+++ __test__/testHandleSubmit.spec.js
@@ -0,0 +1,13 @@
+
+
+
+import { handleSubmit } from "../src/client/js/formHandler";
+import "babel-polyfill";
+
+
+describe("Testing  submit function", () => {
+   
+    test("Testing the handleSubmit() function", () => {
+           
+           expect(handleSubmit).toBeDefined();
+})});
