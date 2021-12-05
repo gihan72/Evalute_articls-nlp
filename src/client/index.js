91b22f4bb3abee578ceff16d2db18231e199fb9c
gihan72 <gihan1972gigi@gmail.com>
2021-12-05 12:14:07 +0200

    Create index.js

---
+++ src/client/index.js
@@ -0,0 +1,14 @@
+// js  & sass files
+
+import { handleSubmit } from './js/formHandler'
+import { urlValidate } from './js/urlChecker'
+import './styles/header.scss'
+import './styles/footer.scss'
+import './styles/resets.scss'
+import './styles/base.scss'
+import './styles/form.scss'
+
+export {
+    urlValidate,
+    handleSubmit
+   }
