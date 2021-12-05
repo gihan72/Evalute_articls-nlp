b169375a3caa8f9aaa835c6b5734cf8a792661d9
gihan72 <gihan1972gigi@gmail.com>
2021-12-05 12:13:19 +0200

    Create testUrlChecker.spec.js

---
+++ __test__/testUrlChecker.spec.js
@@ -0,0 +1,10 @@
+
+import { urlValidate } from "../src/client/js/urlChecker"
+
+
+describe("Testing the submit functionality", () => {
+   
+    test("Testing the urlValidate() function", () => {
+          
+           expect(urlValidate).toBeDefined();
+})});
